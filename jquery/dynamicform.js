5bb62e3422dae4ff74776ac6eb9192a361f9984f
hcoz <hcanozcelik@gmail.com>
2017-09-08 00:12:45 +0300

    form elemnt creation updated

--- jquery/dynamicform.js
+++ jquery/dynamicform.js
@@ -2,24 +2,44 @@
     $(function () {
         var formElements = $('dynamicform');
 
-        for(let formElement of formElements) {
-            var form;
-
+        for (var formElement of formElements) {
             $.getJSON(formElement.dataset.json + '.json', function (formData) {
-                var form = '<form name="' + formData.formSpecs.formName 
-                    + '" method="' + formData.formSpecs.method 
-                    + '" action="' + formData.formSpecs.action + '">';
+                var $form,
+                    $fieldset,
+                    $field,
+                    $label,
+                    $input;
+                
+                $form = $('<form></form>');
+                $form.prop('name', formData.formSpecs.formName);
+                $form.prop('method', formData.formSpecs.method);
+                $form.prop('action', formData.formSpecs.action);
 
-                for(let formRow of formData.formRows) {
-                    form += '<fieldset><label class="' + formRow.label.class + '">'
-                        + formRow.label.text + '</label>' 
-                        + '<input name="' + formRow.input.name 
-                        + '" type="' + formRow.input.type 
-                        + '" value="' + formRow.input.value
-                        + '" class="' + formRow.input.class + '"/></fieldset>';
+                for (var fieldset of formData.formRows) {
+                    $fieldset = $('<fieldset></fieldset>');
+
+                    for (var formRow of fieldset) {
+                        $field = $('<div></div>');
+                        $field.prop('class', formRow.fieldSpecs.class);
+
+                        $label = $('<label></label>');
+                        $label.prop('class', formRow.label.class);
+                        $label.prop('for', formRow.label.for);
+
+                        $input = $('<input/>');
+                        $input.prop('id', formRow.input.id);
+                        $input.prop('class', formRow.input.class);
+                        $input.prop('type', formRow.input.type);
+                        
+                        // append label and input to field div
+                        $field.append($label).append($input);
+                        // append field div to fieldset
+                        $fieldset.append($field);
+                    }
+                    // append fieldset to form
+                    $form.append($fieldset);
                 }
-                form += '</form>';
-                $(formElement).replaceWith(form);
+                $(formElement).replaceWith($form);
             });
         }
     });
