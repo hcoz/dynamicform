152509c7934df5516b182a8b0910b023acfd07cc
Halil Can Ozcelik <h.ozcelik@relephant.net>
2017-10-09 15:12:52 +0300

    gulp compress task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,14 @@ var gulp = require('gulp'),
     minify = require('gulp-minify');
 
 gulp.task('compress', function () {
-
+    gulp.src('lib/*.js')
+        .pipe(minify({
+            ext: {
+                src:'.debug.js',
+                min:'.min.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.min.js']
+        }))
+        .pipe(gulp.dest('dist'))
 });
